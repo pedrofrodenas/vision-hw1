d6006bbedda52035da5e15c25d453148f8287749
pedrofrodenas <pedrofrodenas@gmail.com>
2023-03-04 23:24:39 +0100

    Completed more resize image

--- src/resize_image.c
+++ src/resize_image.c
@@ -19,16 +19,20 @@ image nn_resize(image im, int w, int h)
     Sy = h/im.h;
 
     float Xorig, Yorig = 0;
+    float pixel = 0;
     for (int c=0; c!=im.c; c++)
     {
         for (int y=0; y!=h; y++)
         {
         // Original coordinate system position calculation
+        //   Reescale + Grid Shifting  
         Yorig = (y/Sy)+(0.5/Sy)-0.5;
             for (int x=0; x!=w; x++)
             {
                 Xorig = (x/Sx)+(0.5/Sx)-0.5;
-                set_pixel(imageResized, x, y, c, nn_interpolate(im, Xorig, Yorig, c));
+
+                pixel = nn_interpolate(im, Xorig, Yorig, c);
+                set_pixel(imageResized, x, y, c, pixel);
             }
         }
     }
@@ -38,12 +42,58 @@ image nn_resize(image im, int w, int h)
 float bilinear_interpolate(image im, float x, float y, int c)
 {
     // TODO
-    return 0;
+    int x1 = floor(x);
+    int x2 = ceil(x);
+    int y1 = floor(y);
+    int y2 = ceil(y);
+
+    // Our padding
+    // x1 = (x1 < 0) ? 0:x1;
+    // y1 = (y1 < 0) ? 0:y1;
+
+    // Get sorrounding pixels
+    float v1 = get_pixel(im, x1, y1, c);
+    float v2 = get_pixel(im, x2, y1, c);
+    float v3 = get_pixel(im, x1, y2, c);
+    float v4 = get_pixel(im, x2, y2, c);
+
+    float d1,d2,d3,d4,q1,q2,q=0;
+
+    d1 = x-x1;
+    d2 = x2-x;
+
+    q1 = v1*d2+v2*d1;
+    q2 = v3*d2+v4*d1;
+
+    d3 = y-y1;
+    d4 = y2-y;
+
+    q = q1*d4+q2*d3;
+    return q;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+	// TODO
+	image imageResized = make_image(w, h, im.c);
+    
+	// Get resizing coeficients
+	float Sx, Sy = 0;
+	Sx = w/im.w;
+	Sy = h/im.h;
+    float Xorig, Yorig = 0;
+    for (int c=0; c!=im.c; c++)
+    {
+        for (int y=0; y!=h; y++)
+	    {
+            Yorig = (y/Sy)+(0.5/Sy)-0.5;
+            for (int x=0; x!=w; x++)
+            {
+                Xorig = (x/Sx)+(0.5/Sx)-0.5;
+                set_pixel(imageResized, x, y, c, bilinear_interpolate(im, Xorig, Yorig, c));  
+            } 
+        }
+    }
+	return imageResized;
 }
 
